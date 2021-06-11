4f620512d8fa9e93986cb015abef626239acb0df
Carlos Pratt <carlos.pratt@resser.com>
2021-06-11 17:20:42 -0500

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-
-
+const cors = require('cors')({ origin: true });
 app.get('/', (req, res) =>{
-    res.send("Welcome :D");
+    cors(req, res, () => {
+        res.send("Welcome :D");
+    })
+   
 });
 
 const port = process.env.port || 4500
