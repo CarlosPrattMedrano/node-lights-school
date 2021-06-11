69c03fbe9a0b9f88002e4ec3e39a6b10ce8de6a3
Carlos Pratt <carlos.pratt@resser.com>
2021-06-11 17:45:10 -0500

    gsd

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
 app.get('/', (req, res) =>{
     res.send("Welcomssssssssss");
 });
