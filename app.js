bfc28142f40bcaa29ac34dd27e762f59e3018a26
Carlos Pratt <carlos.pratt@resser.com>
2021-06-11 17:30:25 -0500

    Changed: cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 
+app.AllowAnyOrigin();  
 app.get('/', (req, res) =>{
     res.send("Welcome :D");
 });
